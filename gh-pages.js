8fd8e9ded9119482890473681600c66d56ab3233
Adam Markosian <amarkosian@gmail.com>
2020-10-18 13:37:56 -0700

    Removing usages of jquery and bootstrap

--- gh-pages.js
+++ gh-pages.js
@@ -1,15 +1,21 @@
 function golStart() {
+    let button = document.getElementById('startstop');
     gol.start();
-    $('#startstop').html('Stop');
-    $('#startstop').toggleClass('btn-success btn-danger');
+    button.innerHTML = "Stop";
+    button.classList.add('button-start-off');
+    button.classList.remove('button-start-on');
+    //$('#startstop').toggleClass('btn-success btn-danger');
 }
 function golStop() {
+    let button = document.getElementById('startstop');
     gol.stop();
     if (typeof golInterval === 'number') {
         window.clearInterval(golInterval);
     }
-    $('#startstop').html('Start');
-    $('#startstop').toggleClass('btn-success btn-danger');
+    button.innerHTML = "Start";
+    button.classList.add('button-start-on');
+    button.classList.remove('button-start-off');
+    //$('#startstop').toggleClass('btn-success btn-danger');
 }
 function golStep() {
     if ($('#startstop').html() === 'Stop') {
@@ -18,17 +24,23 @@ function golStep() {
     gol.step();
 }
 function golCrawlers() {
-	if (typeof golInterval === 'number') {
+	let cornersEl = document.querySelector('.corners');
+    let corners = Array.from(cornersEl.getElementsByTagName('input'));
+    let auto = document.getElementById('auto').checked;
+
+    corners.forEach(function(c){
+        gol.cornerCrawlers(c.id);
+    });
+
+    if (typeof golInterval === 'number') {
 		window.clearInterval(golInterval);
 	}
-	$('.corners input:checked').each(function(){
-        gol.cornerCrawlers($(this).attr('id'));
-    });
-	if ($('#auto').is(':checked')) {
+    
+	if (auto) {
 		golInterval = window.setInterval(function(){
-			$('.corners input:checked').each(function(){
-	        	gol.cornerCrawlers($(this).attr('id'));
-	    	});
+            corners.forEach(function(c){
+                gol.cornerCrawlers(c.id);
+            });
 		}, 4000);
 	}
 }
@@ -36,115 +48,112 @@ function golCrawlers() {
 var config;
 var golInterval;
 var gol;
-	
-$(document).ready(function() {
-	config = {
-	    width: $('#width').val(),
-	    height: $('#height').val(),
-	    element: document.getElementById('GOL_Board'),
-	    speed: $('#speed').val(),
-	    cellWidth: 12,
-	    cellHeight: 12
-	};
-	gol = new GameOfLife(config);
 
-    gol.start();
-	
-	$('#auto').click();
-	golCrawlers();
+window.onload = function() {
+    let patternsForm = document.getElementById('patterns');
 
-    $('#startstop').click(function(event){
-        if ($(this).html() === 'Start') {
+    patternsForm.addEventListener("submit", function(event) {
+         event.preventDefault();
+    }, false);
+
+
+    document.getElementById('startstop').addEventListener("click", function(event) {
+        event.preventDefault();
+        if (this.innerHTML === 'Start') {
             golStart();
         }
         else {
             golStop();
         }
-    });
-    $('#step').click(function(){
-        golStep();
-    });
-    $('#clear').click(function(){
-        if ($('#startstop').html() === 'Stop') {
+    }, false);
+
+    document.getElementById('speed').addEventListener('change', function(event) {
+        gol.speed = parseInt(this.value, 10);
+        gol.start();
+    }, false);
+
+
+    document.getElementById('clear').addEventListener("click", function(event) {
+        if (document.getElementById('startstop').innerHTML === 'Stop') {
             golStop()
         }
         gol.clear();
-    });
-    $('#crawlers').on('click', function(event){
-        event.preventDefault();
-        golCrawlers();
-    });
-    
-    $('#queenbee').on('click', function(event){
-        event.preventDefault();
+    }, false);
+
+    document.getElementById('resize').addEventListener("click", function(event) {
+       event.preventDefault();
+        golStop();
+        gol.clear();
+        gol = new GameOfLife({
+            width: document.getElementById('width').value,
+            height: document.getElementById('height').value,
+            element: document.getElementById('GOL_Board'),
+            speed: document.getElementById('speed').value,
+            cellWidth: 12,
+        cellHeight: 12
+        });
+    }, false);
+
+
+    document.getElementById('queenbee').addEventListener("click", function(event) {
         var pattern = gol.patterns.queenbee;
         var x = parseInt((gol.cols / 2) - pattern[0].length / 2, 10);
         var y = parseInt((gol.rows / 2) - pattern.length / 2, 10);
         
         gol.place([x,y], pattern);
         gol.updateBoard();
-    });
-    
-    $('#captivequeen').on('click', function(event){
-        event.preventDefault();
+    }, false);
+
+    document.getElementById('captivequeen').addEventListener("click", function(event) {
         var pattern = gol.patterns.captivequeen;
         var x = parseInt((gol.cols / 2) - pattern[0].length / 2, 10);
         var y = parseInt((gol.rows / 2) - pattern.length / 2, 10);
         
         gol.place([x,y], pattern);
         gol.updateBoard();
-    });
-    
-    $('#crab').on('click', function(event){
-        event.preventDefault();
-        var pattern = gol.patterns.crab;
-        var x = parseInt((gol.cols / 2) - pattern[0].length / 2, 10);
-        var y = parseInt((gol.rows / 2) - pattern.length / 2, 10);
-        
-        gol.place([x,y], pattern);
-        gol.updateBoard();
-    });
-    
-    $('#pentadecathlon').on('click', function(event){
-        event.preventDefault();
+    }, false);
+
+    document.getElementById('pentadecathlon').addEventListener("click", function(event) {
         var pattern = gol.patterns.pentadecathlon;
         var x = parseInt((gol.cols / 2) - pattern[0].length / 2, 10);
         var y = parseInt((gol.rows / 2) - pattern.length / 2, 10);
         
         gol.place([x,y], pattern);
         gol.updateBoard();
-    });
-    
-    $('#toad').on('click', function(event){
-        event.preventDefault();
+    }, false);
+
+    document.getElementById('crab').addEventListener("click", function(event) {
+        var pattern = gol.patterns.crab;
+        var x = parseInt((gol.cols / 2) - pattern[0].length / 2, 10);
+        var y = parseInt((gol.rows / 2) - pattern.length / 2, 10);
+        
+        gol.place([x,y], pattern);
+        gol.updateBoard();
+    }, false);
+
+    document.getElementById('toad').addEventListener("click", function(event) {
         var pattern = gol.patterns.toad;
         var x = parseInt((gol.cols / 2) - pattern[0].length / 2, 10);
         var y = parseInt((gol.rows / 2) - pattern.length / 2, 10);
         
         gol.place([x,y], pattern);
         gol.updateBoard();
-    });
+    }, false);
+
+    document.getElementById('crawlers').addEventListener("click", function(event) {
+        golCrawlers();
+    }, false);
 
 
-
-    $('#speed').on('change', function(){
-        gol.speed = parseInt($(this).val(), 10);
-        gol.start();
-    });
-    $('#resize').click(function(event){
-    	event.preventDefault();
-        golStop();
-        gol.clear();
-        gol = new GameOfLife({
-            width: $('#width').val(),
-            height: $('#height').val(),
-            element: document.getElementById('GOL_Board'),
-            speed: $('#speed').val(),
-            cellWidth: 12,
-	    cellHeight: 12
-        });
-        gol.cornerCrawlers();
-    });
-    
-    $('#startstop').click();
-});
+    config = {
+        width: document.getElementById('width').value,
+        height: document.getElementById('height').value,
+        element: document.getElementById('GOL_Board'),
+        speed: document.getElementById('speed').value,
+        cellWidth: 12,
+        cellHeight: 12
+    };
+    gol = new GameOfLife(config);
+    gol.start();
+    golCrawlers();
+};
